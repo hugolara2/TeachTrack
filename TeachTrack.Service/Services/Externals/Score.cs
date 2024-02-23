38f971a6b64f5174ae3bb2b090dfb93f76321633
Hugo Lara <hugo.alex200197@outlook.com>
2024-02-23 14:08:59 -0600

    Solving Typo

--- TeachTrack.Service/Services/Externals/Score.cs
+++ TeachTrack.Service/Services/Externals/Score.cs
@@ -1,6 +1,6 @@
 namespace TeachTrack.Service.Services.Externals;
 
 public class Score {
-   public readonly float MinimunScore = 150f;
+   public readonly float MinimumScore = 150f;
    public readonly float MaximumScore = 270f;
 }
