524381faa35b635ed2f7a913430072583d2d8e20
Hugo Lara <hugo.alex200197@outlook.com>
2024-05-02 13:14:15 -0600

    little refactor

---
+++ TeachTrack.Api/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeachTrack.Model.DBContext;
+using TeachTrack.Model.Models;
+
+namespace TeachTrack.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentController : ControllerBase {
+   private TeachTrackContext _context;
+
+   public StudentController(TeachTrackContext context) {
+      _context = context;
+   }
+
+   [HttpGet]
+   public async Task<IEnumerable<Student>> Get() => 
+      await _context.Students.ToListAsync();
+}
