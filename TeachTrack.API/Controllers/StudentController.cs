ab12e3766ecb2daa3a5fa74b2768e4e5fd355e78
Hugo Lara <hugo.alex200197@outlook.com>
2024-08-07 19:43:46 -0600

    Cambios y librerias

---
+++ TeachTrack.API/Controllers/StudentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeachTrack.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentController : ControllerBase {
+
+   [HttpGet]
+   public string Get() => "Hola mundo";
+}
